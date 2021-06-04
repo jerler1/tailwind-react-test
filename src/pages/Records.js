7e4d0f383c2d86c644aa387821d2a83f8ec965f0
Bunks <busk_1@yahoo.com>
2021-06-04 16:57:37 -0400

    added the form on the page

--- src/pages/Records.js
+++ src/pages/Records.js
@@ -3,12 +3,18 @@ import { Text } from '@chakra-ui/react';
 import api from "../api/index";
 import RecordsTable from "../components/RecordsTable/RecordsTable";
 import { useQuery } from "react-query";
+import RecordsForm from "../components/RecordsForm";
 
 const fetchRecords = async () => await api.index();
 
 
 const Records = () => {
     const { status, data, error } = useQuery("records", fetchRecords);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(new FormData(event.target));
+      };
 
     switch (status) {
         case "loading":
@@ -19,6 +25,7 @@ const Records = () => {
             return (
                 <main className="container mx-auto">
                     <RecordsTable records={data} />
+                    <RecordsForm handler={handleSubmit} />
                 </main>
          );
     }
