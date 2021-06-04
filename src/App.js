9ff1f69cea0ec8a80cfc617b0cb54d530c33688b
Bunks <busk_1@yahoo.com>
2021-06-04 13:31:32 -0400

    de-referenced index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import React from 'react';
-import { Header, Footer } from "components";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/Records";
 import './index.css';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Header/>
+      <HomePage />
       <Footer/>
     </ChakraProvider>
   );
---
+++ src/pages/Records.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Records = () => {
+    return (
+        <main className="container mx-auto">
+            Records!!!!
+        </main>
+    );
+};
+
+export default Records;
